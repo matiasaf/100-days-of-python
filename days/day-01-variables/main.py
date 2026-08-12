8c8ffa803c6ed18c74190d332d92245ad88ba96d
matiasaf <fernandez.amatias@gmail.com>
2026-08-12 14:23:45 -0300

    Implement band name generator and tip calculator functionality

--- days/day-01-variables/main.py
+++ days/day-01-variables/main.py
@@ -2,7 +2,17 @@
 
 
 def main() -> None:
-    print("Day 01 — Variables — Band Name Generator")
+
+    print('Welcome to the band generator')
+    city_name = input("What is the name of the city you grew up in?\n")
+    pet_name = input("what is yours pet name?\n")
+    
+    print("Your Band name could be " + city_name + " " + pet_name)
+
+    # def generate_band_name(name: str, last_name: str) -> str:
+    #     return name + " " + last_name
+
+    # print("Day 01 — Variables — Band Name Generator" + " " + generate_band_name(name, last_name))
 
 
 if __name__ == "__main__":
--- days/day-02-data-types-strings/main.py
+++ days/day-02-data-types-strings/main.py
@@ -4,6 +4,20 @@
 def main() -> None:
     print("Day 02 — Data Types & Strings — Tip Calculator")
 
+    # print("Hello"[3])
+    # print(type("Matias"))
+    # print(type(111_999))
+    # print(type(1.3))150
+    # print(type(False))
+    print('Welcome to the tip calculator!')
+    total_bill =  float(input("What was the total bill? "))
+    tip = int(input("How much tip would you like to give? 10, 12, or 15 ? "))
+    people_count = int(input("How many people to split the bill? "))
+
+    total_bill_with_tip_percentage = total_bill + (total_bill * tip / 100);
+    total_bill_per_person = round(total_bill_with_tip_percentage / people_count, 2)
+
+    print(f"Each person sould pay: ${total_bill_per_person}")
 
 if __name__ == "__main__":
     main()
