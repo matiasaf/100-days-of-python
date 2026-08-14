2984beb8168c7cc3dc20aae6798c3df2e2a83f23
matiasaf <fernandez.amatias@gmail.com>
2026-08-14 16:30:01 -0300

    Update Day 05 loop example to find maximum student score

--- days/day-05-loops/main.py
+++ days/day-05-loops/main.py
@@ -1,8 +1,15 @@
-"""Day 05 — Loops — Password Generator"""
+"""Day 05 — Loops"""
 
 
 def main() -> None:
-    print("Day 05 — Loops — Password Generator")
+    student_scores = [66, 78, 44, 91, 77]
+    max_score = 0
+
+    for student in student_scores:
+        if student > max_score:
+            max_score = student
+
+    print(max_score)
 
 
 if __name__ == "__main__":
